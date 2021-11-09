389b7359d5bc4d9eedb3c913f160e4ee11ed0b39
Sebastian Ponce <poncegramming@gmail.com>
2021-11-09 17:27:29 +0000

    bruh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import React from "react";
 
 const index = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Home />
   );
 };
 
